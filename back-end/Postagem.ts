c29293bf18907b361158f83d1880bb58533e5677
PCLira954 <126837594+PCLira954@users.noreply.github.com>
2025-07-19 16:17:12 -0300

    Update Postagem.ts

--- back-end/Postagem.ts
+++ back-end/Postagem.ts
@@ -2,15 +2,19 @@ export class Postagem {
     private id: number;
     private titulo: string;
     private conteudo: string;
+    private autor: string;
     private data: Date;
     private curtidas: number;
+    private comentarios: string[];
 
-    constructor(id: number, titulo: string, conteudo: string, data: Date, curtidas: number) {
+    constructor(id: number, titulo: string, conteudo: string, autor: string, data: Date, curtidas: number) {
         this.id = id;
         this.titulo = titulo;
         this.conteudo = conteudo;
+        this.autor = autor;
         this.data = data;
         this.curtidas = curtidas;
+        this.comentarios = [];
     }
 
     public getId(): number {
@@ -25,11 +29,35 @@ export class Postagem {
         return this.conteudo;
     }
 
+    public getAutor(): string {
+        return this.autor;
+    }
+
     public getData(): Date {
         return this.data;
     }
 
     public getCurtidas(): number {
         return this.curtidas;
+    }
+
+    public getComentarios(): string[] {
+        return this.comentarios;
+    }
+
+    public adicionarComentario(comentario: string): void {
+        this.comentarios.push(comentario);
+    }
+
+    public toJSON(): any {
+        return {
+            id: this.id,
+            titulo: this.titulo,
+            conteudo: this.conteudo,
+            autor: this.autor,
+            data: this.data,
+            curtidas: this.curtidas,
+            comentarios: this.comentarios
+        };
     }
 }
