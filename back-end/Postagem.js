b5a105ff9b1ba8fdd581b54b8a985836a2875711
PCLira954 <126837594+PCLira954@users.noreply.github.com>
2025-07-19 16:16:34 -0300

    Update Postagem.js

--- back-end/Postagem.js
+++ back-end/Postagem.js
@@ -1,29 +1,61 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Postagem = void 0;
+
 class Postagem {
-    constructor(id, titulo, conteudo, data, curtidas) {
+    constructor(id, titulo, conteudo, autor, data, curtidas) {
         this.id = id;
         this.titulo = titulo;
         this.conteudo = conteudo;
+        this.autor = autor;
         this.data = data;
         this.curtidas = curtidas;
+        this.comentarios = [];
     }
+
     getId() {
         return this.id;
     }
+
     getTitulo() {
         return this.titulo;
     }
+
     getConteudo() {
         return this.conteudo;
     }
+
+    getAutor() {
+        return this.autor;
+    }
+
     getData() {
         return this.data;
     }
+
     getCurtidas() {
         return this.curtidas;
     }
+
+    getComentarios() {
+        return this.comentarios;
+    }
+
+    adicionarComentario(comentario) {
+        this.comentarios.push(comentario);
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            titulo: this.titulo,
+            conteudo: this.conteudo,
+            autor: this.autor,
+            data: this.data,
+            curtidas: this.curtidas,
+            comentarios: this.comentarios
+        };
+    }
 }
+
 exports.Postagem = Postagem;
-//# sourceMappingURL=Postagem.js.map
