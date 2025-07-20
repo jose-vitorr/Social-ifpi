a2c7cd2c34b9f8d458fa540a15b439d00a1284a8
PCLira954 <126837594+PCLira954@users.noreply.github.com>
2025-07-20 16:55:02 -0300

    Update Postagem.js

--- back-end/Postagem.js
+++ back-end/Postagem.js
@@ -3,46 +3,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Postagem = void 0;
 
 class Postagem {
-    constructor(id, titulo, conteudo, autor, data, curtidas) {
+    constructor(id, titulo, conteudo, autor, data, curtidas, visualizacoes = 0) {
         this.id = id;
         this.titulo = titulo;
         this.conteudo = conteudo;
         this.autor = autor;
         this.data = data;
         this.curtidas = curtidas;
+        this.visualizacoes = visualizacoes;
         this.comentarios = [];
     }
 
-    getId() {
-        return this.id;
-    }
-
-    getTitulo() {
-        return this.titulo;
-    }
-
-    getConteudo() {
-        return this.conteudo;
-    }
-
-    getAutor() {
-        return this.autor;
-    }
-
-    getData() {
-        return this.data;
-    }
-
-    getCurtidas() {
-        return this.curtidas;
-    }
-
-    getComentarios() {
-        return this.comentarios;
-    }
+    getId() { return this.id; }
+    getTitulo() { return this.titulo; }
+    getConteudo() { return this.conteudo; }
+    getAutor() { return this.autor; }
+    getData() { return this.data; }
+    getCurtidas() { return this.curtidas; }
+    getVisualizacoes() { return this.visualizacoes; }
+    getComentarios() { return this.comentarios; }
 
     adicionarComentario(comentario) {
         this.comentarios.push(comentario);
+    }
+
+    incrementarVisualizacoes() {
+        this.visualizacoes++;
     }
 
     toJSON() {
@@ -53,6 +39,7 @@ class Postagem {
             autor: this.autor,
             data: this.data,
             curtidas: this.curtidas,
+            visualizacoes: this.visualizacoes,
             comentarios: this.comentarios
         };
     }
